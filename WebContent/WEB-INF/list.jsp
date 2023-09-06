b1fa9e385556ca7b9b637b0453e8f956a37d8dc9
red-hi2023 <red.hi2023@gmail.com>
2023-09-06 12:30:10 +0900

    1st

---
+++ WebContent/WEB-INF/list.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %>
+<%@ page import="com.javaex.vo.PersonVo" %>
+
+<%
+	//request의 어트리뷰트 영역에 있는 data를 꺼내온다.
+	
+	
+	List<PersonVo> personList = (List<PersonVo>)request.getAttribute("pList");
+	System.out.println(personList);
+%>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>전화번호부</h1>
+	<h2>리스트</h2>
+   
+	<p>등록된 전화번호 리스트 입니다.</p>
+   
+    <!-- 반복문시작 -->
+	<%for(int i=0; i<personList.size(); i++){ %>
+		<table border="1">
+			<tr>
+				<td>이름(name)</td><td><%=personList.get(i).getName()%></td>
+			</tr>
+	   		<tr>
+				<td>핸드폰(hp)</td><td><%=personList.get(i).getHp()%></td>
+			</tr>
+	   		<tr>
+				<td>회사(company)</td><td><%=personList.get(i).getCompany()%></td>
+			</tr>
+			<tr>
+				<td><a href="">[수정]</a></td>
+				<td><a href="">[삭제]</a></td>
+			</tr>
+		</table>
+		<br>
+	<%} %>	
+	<!-- 반복문 끝 -->
+	
+	<a href="">전화번호 등록폼</a> <br>
+	<br><br><br><br><br><br><br>
+</body>
+</html>
---
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -0,0 +1,62 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.PersonDao;
+import com.javaex.vo.PersonVo;
+
+
+@WebServlet("/PhonebookController")
+public class PhonebookController extends HttpServlet {
+	//필드
+	private static final long serialVersionUID = 1L;
+    
+	//생성자 디폴트 생성자사용 - 삭제
+	
+	//메소드 gs 필요없음
+	
+	//메소드 일반
+	//    get방식으로 요청이 들어왔을때 실행되는 메소드
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("PhonebookController");
+		//파라미터 action의 값을 꺼내온다   업무구분욜
+		String action =request.getParameter("action");
+		System.out.println(action);
+		
+		if(action=="list") {
+			//리스트/////////////////////////////////////////////////////////////////////
+			//1.dao를 통해서 전체 리스트데이터 가져오기
+			PersonDao personDao = new PersonDao();
+			List<PersonVo> personList = personDao.personSelect("");
+			
+			//System.out.println(personList);
+			
+			//리스트 화면(html) + data 응답을 해야된다
+	        //request data를 넣어둔다
+			request.setAttribute("pList", personList);
+			
+			//list.jsp 에게 시킨다 	(포워드)
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp"); //jsp파일 위치를
+			rd.forward(request, response);
+			//////////////////////////////////////////////////////////////////////////////
+		}else {
+			System.out.println("나머지");
+		}
+		
+	}
+
+	//  post방식으로 요청이 들어왔을때 실행되는 메소드
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
